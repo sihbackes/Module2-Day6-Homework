57d5b031c8138b72c8bf9c4aa6f19a6443fe94bd
Simone <simonebackesv@gmail.com>
2022-10-04 13:23:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = "Simone";
+let lastName = "Backes";
+let space = " ";
+let fullName = name + space + lastName;
+
+console.log(`Hello, my name is ${fullName}`);
+
+let age = 32;
+let currentYear = 2022;
+let birthYear = age - currentYear;
+
+console.log(`My birth year is ${birthYear}`);
